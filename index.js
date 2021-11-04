972e74e9c37108cbca70e679340036350d466c7e
Chase Rivera <achaserivera@gmail.com>
2021-11-04 14:53:16 -0700

    added function to display all actions user can do

--- index.js
+++ index.js
@@ -14,3 +14,34 @@ const db = mysql.createConnection(
 );
 
 const query = util.promisify(db.query).bind(db);
+
+const nextAction = async () => {
+    const res = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What is your next action?',
+            name: 'nextAction',
+            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update employee role", "Quit"]
+        }
+    ]);
+    switch (res.nextAction) {
+        case 'View all departments':
+            return viewAllDep();
+        case 'View all roles':
+            return viewAllRoles();
+        case 'View all employees':
+            return viewAllEmp();
+        case 'Add a department':
+            return addDep();
+        case 'Add a role':
+            return addRole();
+        case 'Add an employee':
+            return addEmp();
+        case 'Update an employee role':
+            return updateEmpRole();
+        default:
+            console.log(`\nSeeUnxtTime!\n`);
+            return process.exit(0);
+
+    }
+};
