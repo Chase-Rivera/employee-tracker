39d0314f84f4e6b29b1c99bd1c26a742862b1136
Chase Rivera <achaserivera@gmail.com>
2021-11-06 20:36:46 -0700

    found another dept  switched to dep

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const nextAction = async () => {
 };
 
 const viewAllEmp = () => {
-    query(`SELECT e.id, e.first_name, e.last_name, r.title, r.department, r.salary, m.employee_name AS manager FROM employee e LEFT OUTER JOIN (SELECT r.id, r.title, d.name AS department, r.salary FROM role r JOIN department d ON r.dept_id = d.id) r ON e.role_id = r.id LEFT OUTER JOIN (SELECT id, CONCAT(first_name, " ",last_name) as employee_name FROM employee) m ON e.manager_id = m.id`)
+    query(`SELECT e.id, e.first_name, e.last_name, r.title, r.department, r.salary, m.employee_name AS manager FROM employee e LEFT OUTER JOIN (SELECT r.id, r.title, d.name AS department, r.salary FROM role r JOIN department d ON r.dep_id = d.id) r ON e.role_id = r.id LEFT OUTER JOIN (SELECT id, CONCAT(first_name, " ",last_name) as employee_name FROM employee) m ON e.manager_id = m.id`)
     .then((res) => {
         console.table(res);
         return nextAction();
